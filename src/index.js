1a2a64166d135911838ee4d5e3460c3435696736
Sergey <segany4@gmail.com>
2023-02-02 01:40:41 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    while (n) {
+        if (n < 0) n = n * -1;
+        result = result * 10 + n % 10;
+        n = Math.floor(n / 10);
+    }
+    return result;
 }
